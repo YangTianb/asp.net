7daed2d227e5fb2e727857bac20460699e13a294
bayker <bayker@live.cn>
2015-11-18 08:26:03 +0800

    demolinq

--- CSharp/ExtensionMethod/Program.cs
+++ CSharp/ExtensionMethod/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace 扩展方法学习
+namespace ExtensionMethod
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DemoLinq();
+        }
+
+        private static void DemoLinq()
+        {
+            int[] ints = { 10, 20, 3, 33, 4 };
+            var result = ints.OrderBy(g => g);
+
+            foreach (var i in result)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.ReadLine();
         }
     }
 }
