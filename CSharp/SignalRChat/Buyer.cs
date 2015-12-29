c03ae15f07bbe885bbed5d7f3321c2726ec7c5c9
bayker <bayker@live.cn>
2015-12-29 08:58:16 +0800

    推送更新

---
+++ CSharp/SignalRChat/Buyer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using MessageServer;
+
+namespace SignalRChat
+{
+    public class Buyer : Hub
+    {
+        private const int TakeCount = 20;
+
+        public List<DbBuyer> GetNeastBuyerInfo(string groupName)
+        {
+            //消息分组  
+            Groups.Add(Context.ConnectionId, groupName);
+         
+            //Clients.Group(groupName).addMessage(lisByer);
+            var lisByer = new List<DbBuyer>();
+            var _buyerid = new Random().Next(232432432);
+            lisByer.Add(new DbBuyer() { BuyerUid = _buyerid.ToString(), Nick = "张三", Id = 1 , GoupName=groupName});
+            return lisByer;
+        }
+    }
+}
---
+++ CSharp/SignalRChat/Controllers/BuyerController.cs
@@ -0,0 +1,26 @@
+﻿using MessageServer;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRChat.Controllers
+{
+    public class BuyerController : Controller
+    {
+        // GET: Buyer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sync(string groupName)
+        {
+            //更新组消息
+            GlobalHost.ConnectionManager.GetHubContext<Buyer>().Clients.Group(groupName).updateInfo();
+            return Content("");
+        }
+    }
+}
