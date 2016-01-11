7341d5d989ac8e334646db1188b67bce68cc7b06
bayker <bayker@live.cn>
2016-01-11 09:32:56 +0800

    添加Asp.net identity项目

---
+++ CSharp/MyIdentitySampleTest/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyIdentitySampleTest
+{
+    [Authorize]
+    public class HomeController: Controller
+    {
+        [Authorize(Roles = "Users")]
+        public ActionResult Index()
+        {
+            var user = User.Identity.Name;
+
+            return View();
+        }
+        [Authorize(Roles = "Managers")]
+        public ActionResult Managers()
+        {
+
+            return View();
+        }
+
+    }
+}
